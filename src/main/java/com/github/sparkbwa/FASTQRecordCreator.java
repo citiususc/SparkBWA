e5a424fdcad316e666287b695157a1c6ab403da6
Jose Manuel Abuin Mosquera <abuinjm@gmail.com>
2016-10-26 12:35:53 +0200

    New FASTQ readers are commented and GPL header added

--- src/main/java/com/github/sparkbwa/FASTQRecordCreator.java
+++ src/main/java/com/github/sparkbwa/FASTQRecordCreator.java
@@ -1,42 +1,68 @@
+/**
+ * Copyright 2016 José Manuel Abuín Mosquera <josemanuel.abuin@usc.es>
+ *
+ * <p>This file is part of SparkBWA.
+ *
+ * <p>SparkBWA is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>SparkBWA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with SparkBWA. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.sparkbwa;
 
-import com.clearspring.analytics.util.Lists;
-import org.apache.commons.collections.IteratorUtils;
 import org.apache.spark.api.java.function.Function;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.regex.Pattern;
-
+/**
+ * Class that implements functionality of grouping FASTQ indexed PairRDD
+ * @author Pål Karlsrud
+ * @author Jose M. Abuin
+ */
 public class FASTQRecordCreator implements Function<Iterable<Tuple2<String, Long>>, String> {
-    @Override
-    public String call(Iterable<Tuple2<String, Long>> iterableRecord) throws Exception {
-        String seqName = null;
-        String seq = null;
-        String qual = null;
-        String extraSeqname = null;
 
-        for (Tuple2<String, Long> recordLine : iterableRecord) {
-            Long lineNum = recordLine._2();
-            String line = recordLine._1();
+	@Override
+	public String call(Iterable<Tuple2<String, Long>> iterableRecord) throws Exception {
+		// We create the data to be contained inside the record
+		String seqName 		= null;
+		String seq			= null;
+		String qual			= null;
+		String extraSeqname	= null;
 
-            if (lineNum == 0)
-                seqName = line;
-            else if (lineNum == 1)
-                seq = line;
-            else if (lineNum == 2)
-                extraSeqname = line;
-            else
-                qual = line;
-        }
+		//
+		for (Tuple2<String, Long> recordLine : iterableRecord) {
+			// Keep in mind that records are sorted by key. This is, we have 4 available lines here
+			Long lineNum = recordLine._2();
+			String line = recordLine._1();
 
-        if (seqName != null && seq != null && qual != null && extraSeqname != null) {
-            return String.format("%s\n%s\n%s\n%s\n", seqName, seq, extraSeqname, qual);
-        } else {
-            System.err.println("Malformed record!");
-            System.err.println(String.format("%s\n%s\n%s\n%s\n", seqName, seq, extraSeqname, qual));
-            return null;
-        }
-    }
+			if (lineNum == 0) {
+				seqName = line;
+			}
+			else if (lineNum == 1) {
+				seq = line;
+			}
+			else if (lineNum == 2) {
+				extraSeqname = line;
+			}
+			else {
+				qual = line;
+			}
+		}
+
+		// If everything went fine, we return the current record
+		if (seqName != null && seq != null && qual != null && extraSeqname != null) {
+			return String.format("%s\n%s\n%s\n%s\n", seqName, seq, extraSeqname, qual);
+		}
+		else {
+			System.err.println("Malformed record!");
+			System.err.println(String.format("%s\n%s\n%s\n%s\n", seqName, seq, extraSeqname, qual));
+			return null;
+		}
+	}
 }
--- src/main/java/com/github/sparkbwa/FASTQRecordGrouper.java
+++ src/main/java/com/github/sparkbwa/FASTQRecordGrouper.java
@@ -1,18 +1,45 @@
+/**
+ * Copyright 2016 José Manuel Abuín Mosquera <josemanuel.abuin@usc.es>
+ *
+ * <p>This file is part of SparkBWA.
+ *
+ * <p>SparkBWA is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>SparkBWA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with SparkBWA. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.sparkbwa;
 
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
+/**
+ * Class that implements functionality of indexing FASTQ input lines into groups of 4 (one read)
+ * @author Pål Karlsrud
+ * @author Jose M. Abuin
+ */
 public class FASTQRecordGrouper implements PairFunction<Tuple2<String, Long>, Long, Tuple2<String, Long>> {
 
-    @Override
-    public Tuple2<Long, Tuple2<String, Long>> call(Tuple2<String, Long> recordTuple) throws Exception {
-        String line = recordTuple._1();
-        Long fastqLineNum = recordTuple._2();
+	@Override
+	public Tuple2<Long, Tuple2<String, Long>> call(Tuple2<String, Long> recordTuple) throws Exception {
 
-        Long recordNum = (long) Math.floor(fastqLineNum / 4);
-        Tuple2<String, Long> newRecordTuple = new Tuple2<String, Long>(line, fastqLineNum % 4);
+		// We get string input and line number
+		String line = recordTuple._1();
+		Long fastqLineNum = recordTuple._2();
 
-        return new Tuple2<Long,Tuple2<String, Long>>(recordNum, newRecordTuple);
-    }
+		// We obtain the record number from the line number
+		Long recordNum = (long) Math.floor(fastqLineNum / 4);
+		// We form the pair <String line, Long index inside record (0..3)>
+		Tuple2<String, Long> newRecordTuple = new Tuple2<String, Long>(line, fastqLineNum % 4);
+
+		// We return the data to group <Long record number,<String line, Long line index>>
+		return new Tuple2<Long,Tuple2<String, Long>>(recordNum, newRecordTuple);
+	}
 }
